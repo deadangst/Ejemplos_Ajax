1ff7b12dcb825eeaa44f2010e2f562a80046d737
deadangst <32141400+deadangst@users.noreply.github.com>
2022-11-04 14:57:53 -0600

    Add project files.

---
+++ PL/PG/Home.Master.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using AjaxControlToolkit;
+
+namespace PL.PG
+{
+    public partial class ClaseMaestra : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        } 
+    }
+}
---
+++ PL/PG/Home1.aspx
@@ -0,0 +1,3 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/PG/Home.Master" AutoEventWireup="true" CodeBehind="Home1.aspx.cs" Inherits="PL.PG.Home1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+</asp:Content>
